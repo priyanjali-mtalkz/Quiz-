c63cd74377bec52bb109c0951b6c6887191fcba2
priyanjali-mtalkz <priyanjali.mtalkz@gmail.com>
2023-02-11 02:43:44 +0530

    quizapp

--- Quiz/app.py
+++ Quiz/app.py
@@ -1,9 +1,10 @@
-from flask import *
+from flask import Flask
 from controller import *
 from model.schema import Schema
 import pymongo
 import time
 from flask import request,render_template,redirect,url_for,jsonify
+import random
 
 app = Flask(__name__)
 
@@ -21,15 +22,11 @@ user_name = None
 mobile = None
 level = None
 
+
 @app.route('/',methods=['GET','POST'])
-def home():
-    return  "Welcome"
-
-
-@app.route('/users',methods = ['GET','POST'])
 def register():
     register.start_time = time.time()
-    print(register.start_time)
+    print(start_time)
     try:
         if request.method == 'POST':
             return render_template("signup.html")
@@ -37,6 +34,7 @@ def register():
             return render_template("signup.html")
     except Exception as e:
         print(e)
+
 
 @app.route('/level/quiz',methods = ['GET','POST'])
 def user_level():
@@ -46,27 +44,36 @@ def user_level():
     print(user_level.mobile)
     return render_template("level.html")
 
-@app.route('/play/quiz',methods = ['GET','POST'])
+# @app.route('/play/quiz',methods = ['GET','POST'])
+# def game():
+#     game.level = request.form.get('level')
+#     print(game.level)
+#     for quest in coll.find({'Level':game.level}):
+#         q = quest['questions']
+#         game.q_rand = random.sample(q,5)
+#         #print(quest['questions'])
+#         # coll1.aggregate([{$sample:{size:1}}])
+#         return render_template("quiz.html",question = game.q_rand)
+@app.route('/play/quiz',methods=['GET','POST'])
 def game():
     game.level = request.form.get('level')
-    print(game.level)
-    for quest in coll.find({'Level':game.level}):
+    for quest in coll.find({'Level':game.level}).limit(5):
+        q = quest['questions']
+        game.q_rand = random.sample(q,5)
         #print(quest['questions'])
         # coll1.aggregate([{$sample:{size:1}}])
-        return render_template("quiz.html",question = quest['questions'])
+        return render_template("quiz.html",question = game.q_rand)
+    
 
 
 @app.route('/submit/quiz',methods = ['POST','GET'])
 def submit():
-    ans = 0
-    print(game.level)
-    l = game.level
-    for ques in coll.find({'Level':l}):
-        q = ques['questions']
-        print((q[0]['q_id']))
+        ans = 0
+        #print(game.level)
         try:
-            for dic in q:
-                print("hello")
+            for dic in game.q_rand:
+                # print(dic)
+                #print("hello")
                 dic['q_id'] = str(dic['q_id'])
                 user_answer = request.form.get(dic['q_id'])
                 user_answer = user_answer[:1]
@@ -76,7 +83,7 @@ def submit():
                 #print(correct_ans)
                 if(user_answer.startswith(correct_ans)):
                     ans = ans + 1
-                #print(ans)
+                print(ans)
             submit.score = str(ans)
             return render_template("score.html",score = submit.score)
         except Exception as e:
@@ -84,11 +91,11 @@ def submit():
         return jsonify()
     
 
-        
+       
 @app.route('/play/again',methods = ['GET','POST'])
 def play_again():
     play_again.end_time = time.time()
-    #print(end_time)
+    print(end_time)
     if request.method == 'POST':
         try:
             if (request.form.get('choice') == "YES"):
@@ -99,12 +106,12 @@ def play_again():
             print("----ERROR------",e) 
         return jsonify()
     return jsonify({"Status" : "Thankyou"})
-            
 
+            
 @app.route('/users/get/details')
 def data():
     try:
-        time_taken = play_again.end_time-register.start_time
+        time_taken = play_again.end_time - register.start_time
         user_data = {}
         arr1 = []
         data1 = "User Name"
@@ -121,7 +128,7 @@ def data():
         user_data[data4] = submit.score
 
         data5 = "Timetaken"
-        user_data[data5] = time_taken
+        user_data[data5] = "%.2f" %time_taken
 
         data6 = "User answers"
         user_data[data6] = user_answers
@@ -134,6 +141,7 @@ def data():
         print("error in data function",e)
 
     return jsonify()
+
 
 
 if __name__ == "__main__":
